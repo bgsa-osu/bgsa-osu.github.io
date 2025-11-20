07644276dd8e3371957a554d92aceb42796370a5
Hassan <naimul.hassan273@gmail.com>
2025-11-20 16:56:29 -0500

    updated

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ for (let i = 0; i < headerSmallMenuLinks.length; i++) {
 }
 
 // ---
-const headerLogoConatiner = document.querySelector('.header__logo-container')
+const headerLogoContainer = document.querySelector('.header__logo-container')
 
 headerLogoConatiner.addEventListener('click', () => {
   location.href = 'index.html'
