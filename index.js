eef5487c4046e40355adec7a7aae909f129c96d1
K M NAIMUL HASSAN <kmnaimulhassan@Mac.lan>
2025-11-30 09:05:35 -0500

    updated

--- index.js
+++
@@ -1,7 +0,0 @@
-const ham = document.querySelector('.header__main-ham-menu-cont');
-const mobileMenu = document.getElementById('mobile-menu');
-
-ham.addEventListener('click', () => {
-  mobileMenu.style.display =
-    mobileMenu.style.display === "block" ? "none" : "block";
-});
